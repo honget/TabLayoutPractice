31c463f958cb9a325e90647e7349687e005c904f
Hong <bilre84@naver.com>
2019-05-18 11:02:26 +0900

    탭 레이아웃 활용 연습

--- app/src/main/java/com/example/tablayoutpractice/MainActivity.java
+++ app/src/main/java/com/example/tablayoutpractice/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.tablayoutpractice;
 
 import android.databinding.DataBindingUtil;
+import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Layout;
@@ -28,7 +29,22 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void setValues() {
-
+        act.tabLayout.addTab(act.tabLayout.newTab().setText("홈"));
+        act.tabLayout.addTab(act.tabLayout.newTab().setText("채팅목록").setIcon(R.mipmap.ic_launcher));
+        act.tabLayout.addTab(act.tabLayout.newTab().setText("검색"));
+        act.tabLayout.addTab(act.tabLayout.newTab().setText("더보기"));
+        act.tabLayout.addTab(act.tabLayout.newTab().setText("더보기"));
+        act.tabLayout.addTab(act.tabLayout.newTab().setText("더보기"));
+        act.tabLayout.addTab(act.tabLayout.newTab().setText("더보기"));
+        act.tabLayout.addTab(act.tabLayout.newTab().setText("더보기"));
+        act.tabLayout.addTab(act.tabLayout.newTab().setText("더보기"));
+        act.tabLayout.addTab(act.tabLayout.newTab().setText("더보기"));
+        act.tabLayout.addTab(act.tabLayout.newTab().setText("더보기"));
+        act.tabLayout.addTab(act.tabLayout.newTab().setText("더보기"));
+        act.tabLayout.addTab(act.tabLayout.newTab().setText("더보기"));
+        act.tabLayout.addTab(act.tabLayout.newTab().setText("더보기"));
+        act.tabLayout.addTab(act.tabLayout.newTab().setText("더보기"));
+        act.tabLayout.addTab(act.tabLayout.newTab().setText("더보기"));
     }
 
     @Override
