daf9bdbef8209412199be97efa487d9589d3aa85
Hong <bilre84@naver.com>
2019-05-18 11:19:08 +0900

    탭 View를 커스텀 마무리

--- app/src/main/java/com/example/tablayoutpractice/MainActivity.java
+++ app/src/main/java/com/example/tablayoutpractice/MainActivity.java
@@ -5,6 +5,9 @@ import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.tablayoutpractice.databinding.ActivityMainBinding;
 
@@ -33,18 +36,18 @@ public class MainActivity extends BaseActivity {
         act.tabLayout.addTab(act.tabLayout.newTab().setText("채팅목록").setIcon(R.mipmap.ic_launcher));
         act.tabLayout.addTab(act.tabLayout.newTab().setText("검색"));
         act.tabLayout.addTab(act.tabLayout.newTab().setText("더보기"));
-        act.tabLayout.addTab(act.tabLayout.newTab().setText("더보기"));
-        act.tabLayout.addTab(act.tabLayout.newTab().setText("더보기"));
-        act.tabLayout.addTab(act.tabLayout.newTab().setText("더보기"));
-        act.tabLayout.addTab(act.tabLayout.newTab().setText("더보기"));
-        act.tabLayout.addTab(act.tabLayout.newTab().setText("더보기"));
-        act.tabLayout.addTab(act.tabLayout.newTab().setText("더보기"));
-        act.tabLayout.addTab(act.tabLayout.newTab().setText("더보기"));
-        act.tabLayout.addTab(act.tabLayout.newTab().setText("더보기"));
-        act.tabLayout.addTab(act.tabLayout.newTab().setText("더보기"));
-        act.tabLayout.addTab(act.tabLayout.newTab().setText("더보기"));
-        act.tabLayout.addTab(act.tabLayout.newTab().setText("더보기"));
-        act.tabLayout.addTab(act.tabLayout.newTab().setText("더보기"));
+        act.tabLayout.addTab(act.tabLayout.newTab().setCustomView(createCustomTabViwe("커스텀", "")));
+        act.tabLayout.addTab(act.tabLayout.newTab().setCustomView(createCustomTabViwe("커스텀2", "")));
+    }
+
+    public View createCustomTabViwe(String tabName, String imgUrl){
+
+        View tabView = LayoutInflater.from(mContext).inflate(R.layout.custom_tab, null);
+
+        TextView nameTxt = tabView.findViewById(R.id.nameTxtV);
+        nameTxt.setText(tabName);
+
+        return tabView;
     }
 
     @Override
